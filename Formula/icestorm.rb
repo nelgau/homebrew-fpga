59e9eb6dd88337b8089df174a3bf50d95a816df9
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-09-30 14:27:35 -0700

    Don't use the SymbiFlow icestorm fork either

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -1,7 +1,7 @@
 class Icestorm < Formula
   desc "Tools for documenting and working with iCE40 FPGA files"
-  homepage "https://github.com/cliffordwolf/icestorm"
-  head "https://github.com/cliffordwolf/icestorm.git"
+  homepage "https://github.com/YosysHQ/icestorm"
+  head "https://github.com/YosysHQ/icestorm.git"
 
   depends_on "pkg-config" => :build
   depends_on "gnu-sed" => :build
