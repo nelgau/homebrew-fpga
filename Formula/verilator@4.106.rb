c331fb3e15fc3ecee79613d290a2274478c51142
Nelson Gauthier <nelson.gauthier@gmail.com>
2022-10-22 13:57:51 -0700

    Add formula for verilator@4.106
    This formula is intended to install the latest version of verilator
    that's compatible with cocotb. If the maintainers resolve the VPI
    callback issue, this formula will be obsolete.

---
+++ Formula/verilator@4.106.rb
@@ -0,0 +1,58 @@
+class VerilatorAT4106 < Formula
+  desc "Verilog simulator"
+  homepage "https://www.veripool.org/wiki/verilator"
+  url "https://www.veripool.org/ftp/verilator-4.106.tgz"
+  sha256 "e31f43fcd984f17c0f10fb4e26bed4203919a5c8c9f1beafc9aafcb5f85d72b7"
+  license any_of: ["LGPL-3.0-only", "Artistic-2.0"]
+
+  livecheck do
+    url "https://github.com/verilator/verilator.git"
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "perl"
+  uses_from_macos "python", since: :catalina
+
+  skip_clean "bin" # Allows perl scripts to keep their executable flag
+
+  def install
+    system "autoconf"
+    system "./configure", "--prefix=#{prefix}"
+    # `make` and `make install` need to be separate for parallel builds
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.v").write <<~EOS
+      module test;
+         initial begin $display("Hello World"); $finish; end
+      endmodule
+    EOS
+    (testpath/"test.cpp").write <<~EOS
+      #include "Vtest.h"
+      #include "verilated.h"
+      int main(int argc, char **argv, char **env) {
+          Verilated::commandArgs(argc, argv);
+          Vtest* top = new Vtest;
+          while (!Verilated::gotFinish()) { top->eval(); }
+          delete top;
+          exit(0);
+      }
+    EOS
+    system "/usr/bin/perl", bin/"verilator", "-Wall", "--cc", "test.v", "--exe", "test.cpp"
+    cd "obj_dir" do
+      system "make", "-j", "-f", "Vtest.mk", "Vtest"
+      expected = <<~EOS
+        Hello World
+        - test.v:2: Verilog $finish
+      EOS
+      assert_equal expected, shell_output("./Vtest")
+    end
+  end
+end
